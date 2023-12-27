a5cd69135578160305227902b826d45182a857b1
Steven Parker <sparker@calquake.com>
2023-12-27 08:41:37 -0800

    initial commit

---
+++ OnnxObjectDetection/ML/DataModels/ImageInputData.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.ML.Data;
+using Microsoft.ML.Transforms.Image;
+using System.Drawing;
+
+namespace OnnxObjectDetection
+{
+    public struct ImageSettings
+    {
+        public const int imageHeight = 416;
+        public const int imageWidth = 416;
+    }
+
+    public class ImageInputData
+    {
+        [ImageType(ImageSettings.imageHeight, ImageSettings.imageWidth)]
+        public MLImage Image { get; set; }
+    }
+}
---
+++ OnnxObjectDetectionWeb/Controllers/ObjectDetectionController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OnnxObjectDetectionWeb.Infrastructure;
+using OnnxObjectDetectionWeb.Services;
+using OnnxObjectDetectionWeb.Utilities;
+using OnnxObjectDetection;
+using Microsoft.ML.Data;
+using Microsoft.ML.Trainers;
+using Microsoft.ML;
+
+namespace OnnxObjectDetectionWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ObjectDetectionController : ControllerBase
+    {
+        private readonly string _imagesTmpFolder;        
+
+        private readonly ILogger<ObjectDetectionController> _logger;
+        private readonly IObjectDetectionService _objectDetectionService;
+
+        private string base64String = string.Empty;
+        public ObjectDetectionController(IObjectDetectionService ObjectDetectionService, ILogger<ObjectDetectionController> logger, IImageFileWriter imageWriter) //When using DI/IoC (IImageFileWriter imageWriter)
+        {
+            //Get injected dependencies
+            _objectDetectionService = ObjectDetectionService;
+            _logger = logger;
+            _imagesTmpFolder = CommonHelpers.GetAbsolutePath(@"../../../ImagesTemp");
+        }
+
+        public class Result
+        {
+            public string imageString { get; set; }
+        }
+
+        [HttpGet()]
+        public IActionResult Get([FromQuery]string url)
+        {
+            string imageFileRelativePath = @"../../../assets" + url;
+            string imageFilePath = CommonHelpers.GetAbsolutePath(imageFileRelativePath);
+            try
+            {
+                MLImage image = MLImage.CreateFromFile(imageFilePath);
+
+                //Set the specific image data into the ImageInputData type used in the DataView
+                ImageInputData imageInputData = new ImageInputData { Image = image };
+
+                //Detect the objects in the image                
+                var result = DetectAndPaintImage(imageInputData,imageFilePath);
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                _logger.LogInformation("Error is: " + e.Message);
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [Route("IdentifyObjects")]
+        public async Task<IActionResult> IdentifyObjects(IFormFile imageFile)
+        {
+            if (imageFile.Length == 0)
+                return BadRequest();
+            try
+            {
+                MemoryStream imageMemoryStream = new MemoryStream();
+                await imageFile.CopyToAsync(imageMemoryStream);                
+
+                //Check that the image is valid
+                byte[] imageData = imageMemoryStream.ToArray();
+                if (!imageData.IsValidImage())
+                    return StatusCode(StatusCodes.Status415UnsupportedMediaType);
+                imageMemoryStream.Position = 0;
+                //Convert to Image
+                MLImage image = MLImage.CreateFromStream(imageMemoryStream);
+
+                string fileName = string.Format("{0}.Jpeg", image.GetHashCode());
+                string imageFilePath = Path.Combine(_imagesTmpFolder, fileName);
+                //save image to a path
+                image.Save(imageFilePath);
+
+                _logger.LogInformation($"Start processing image...");
+
+                //Measure execution time
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+
+                //Set the specific image data into the ImageInputData type used in the DataView
+                ImageInputData imageInputData = new ImageInputData { Image = image };
+
+                //Detect the objects in the image                
+                var result = DetectAndPaintImage(imageInputData, imageFilePath);
+
+                //Stop measuring time
+                watch.Stop();
+                var elapsedMs = watch.ElapsedMilliseconds;
+                _logger.LogInformation($"Image processed in {elapsedMs} miliseconds");
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                _logger.LogInformation("Error is: " + e.Message);
+                return BadRequest();
+            }
+        }
+
+        private Result DetectAndPaintImage(ImageInputData imageInputData, string imageFilePath)
+        {
+            //Predict the objects in the image
+            _objectDetectionService.DetectObjectsUsingModel(imageInputData);
+            var img = _objectDetectionService.DrawBoundingBox(imageFilePath);
+
+            using (MemoryStream m = new MemoryStream())
+            {
+                img.Save(m, img.RawFormat);
+                byte[] imageBytes = m.ToArray();
+
+                // Convert byte[] to Base64 String
+                base64String = Convert.ToBase64String(imageBytes);
+                var result = new Result { imageString = base64String };
+                return result;
+            }
+        }
+
+        //        public void SimpleEndToEndOnnxConversionTest()
+        //        {
+        //            // Step 1: Create and train a ML.NET pipeline.
+        //            var trainDataPath = GetDataPath(TestDatasets.generatedRegressionDataset.trainFilename);
+        //            var mlContext = new MLContext(seed: 1);
+        //            var data = mlContext.Data.LoadFromTextFile<AdultData>(trainDataPath,
+        //                separatorChar: ';'
+        //,
+        //                hasHeader: true);
+        //            var cachedTrainData = mlContext.Data.Cache(data);
+        //            var dynamicPipeline =
+        //                mlContext.Transforms.NormalizeMinMax("FeatureVector")
+        //                .AppendCacheCheckpoint(mlContext)
+        //                .Append(mlContext.Regression.Trainers.Sdca(new SdcaRegressionTrainer.Options()
+        //                {
+        //                    LabelColumnName = "Target",
+        //                    FeatureColumnName = "FeatureVector",
+        //                    NumberOfThreads = 1
+        //                }));
+
+        //            var onnxFileName = "model.onnx";
+        //            var subDir = Path.Combine("Onnx", "Regression", "Adult");
+        //            var onnxTextName = "SimplePipeline.txt";
+        //        }
+
+        //        protected static string DataDir { get; }
+
+        //        public static string GetDataPath(string name)
+        //        {
+        //            if (string.IsNullOrWhiteSpace(name))
+        //                return null;
+        //            return Path.GetFullPath(Path.Combine(DataDir, name));
+        //        }
+    }
+}
